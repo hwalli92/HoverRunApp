05fb404217d0a86fc54ebe1f9a9cd2ae3db80fd5
Hasnainali Walli <hwalli92@gmail.com>
2021-05-30 01:46:57 +0200

    Add workout functionality to watch contentView

--- HoverRun WatchKit Extension/ContentView.swift
+++ HoverRun WatchKit Extension/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject var workoutSession: WorkoutManager
+    @State var workoutInProgress = false
+    
+    let screenHeight = WKInterfaceDevice.current().screenBounds.size.height
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        PageView(workoutInProgress: self.$workoutInProgress)
+        .opacity(self.workoutInProgress ? 1 : 0)
+        .animation(.easeInOut(duration: 0.3))
+        .overlay(
+            StartView(startAction: startAction)
+                .offset(CGSize(width: 0, height: self.workoutInProgress ? self.screenHeight : 0))
+                .animation(.easeInOut(duration: 0.3)))
+    }
+    
+    func startAction() {
+        workoutSession.startWorkout()
+        withAnimation {
+            workoutInProgress = true
+        }
     }
 }
 
